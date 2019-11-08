01908312dde14070b894d0cebccff30ae7779b2c
Rodolfo Totaro <rodolfo_totaro@yahoo.it>
2019-11-08 22:27:53 +0100

    GEOMESA-2737 Test CQEngine STRtree and Quadtree indexes support
    Signed-off-by: Rodolfo Totaro <rodolfo_totaro@yahoo.it>

--- geomesa-memory/geomesa-cqengine/src/test/scala/org/locationtech/geomesa/memory/cqengine/utils/GeoCQEngineTest.scala
+++ geomesa-memory/geomesa-cqengine/src/test/scala/org/locationtech/geomesa/memory/cqengine/utils/GeoCQEngineTest.scala
@@ -6,45 +6,24 @@
   * http://www.opensource.org/licenses/apache2.0.php.
   * **********************************************************************/
 
-/** *********************************************************************
-  * Copyright (c) 2013-2019 Commonwealth Computer Research, Inc.
-  * All rights reserved. This program and the accompanying materials
-  * are made available under the terms of the Apache License, Version 2.0
-  * which accompanies this distribution and is available at
-  * http://www.opensource.org/licenses/apache2.0.php.
-  * **********************************************************************/
-
 package org.locationtech.geomesa.memory.cqengine.utils
 
-import java.lang
-
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.log4j
-import org.apache.log4j.spi.LoggingEvent
-import org.apache.log4j.{AppenderSkeleton, Category, Level, LogManager, Logger}
 import org.junit.runner.RunWith
-import org.locationtech.geomesa.memory.cqengine.{GeoCQEngine, index}
+import org.locationtech.geomesa.memory.cqengine.GeoCQEngine
 import org.locationtech.geomesa.memory.cqengine.attribute.{GeoIndexType, STRtreeIndexParam}
-import org.locationtech.geomesa.memory.cqengine.index.{AbstractGeoIndex, BucketGeoIndex, QuadTreeGeoIndex, STRtreeGeoIndex}
 import org.locationtech.geomesa.memory.cqengine.utils.SampleFeatures._
 import org.locationtech.geomesa.utils.collection.SelfClosingIterator
-import org.locationtech.geomesa.utils.index.{SpatialIndex, WrappedQuadtree, WrappedSTRtree}
-import org.opengis.feature.simple.SimpleFeature
 import org.opengis.filter.Filter
-import org.opengis.geometry.Geometry
 import org.specs2.matcher.MatchResult
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import org.specs2.specification.core.{Fragment, Fragments}
 
-import scala.reflect.ClassTag
-
 @RunWith(classOf[JUnitRunner])
 class GeoCQEngineTest extends Specification with LazyLogging {
 
   import SampleFilters._
-
-  System.setProperty("GeoCQEngineDebugEnabled", "true")
 
   val feats = (0 until 1000).map(SampleFeatures.buildFeature)
 
@@ -75,17 +54,10 @@ class GeoCQEngineTest extends Specification with LazyLogging {
     SelfClosingIterator(cq.query(filter)).size
   }
 
-  def checkFilter(filter: Filter, cq: GeoCQEngine, spatialIndex: Option[Class[_]]): MatchResult[Int] = {
+  def checkFilter(filter: Filter, cq: GeoCQEngine): MatchResult[Int] = {
     val gtCount = getGeoToolsCount(filter)
 
     val cqCount = getCQEngineCount(filter, cq)
-
-
-    val lastIndexUsed = GeoCQEngine.getLastIndexUsed()
-    if (spatialIndex.isDefined) {
-      val expected = spatialIndex.get
-      lastIndexUsed must beAnInstanceOf expected
-    }
 
     val msg = s"GT: $gtCount CQ: $cqCount Filter: $filter"
     if (gtCount == cqCount)
@@ -97,42 +69,28 @@ class GeoCQEngineTest extends Specification with LazyLogging {
     cqCount must equalTo(gtCount)
   }
 
-  def buildFilterTests(name: String, filters: Seq[Filter], checkFilterType: Boolean): Seq[Fragment] = {
-
-    var spatialIndex: Option[Class[_] ]= Option.empty
+  def buildFilterTests(name: String, filters: Seq[Filter]): Seq[Fragment] = {
     for (f <- filters) yield {
 
       s"return correct number of results for $name filter $f (geo-only index)" >> {
-        checkFilter(f, cqNoIndexes, spatialIndex)
+        checkFilter(f, cqNoIndexes)
       }
       s"return correct number of results for $name filter $f (various indices)" >> {
-        if (checkFilterType) {
-          spatialIndex = Option.apply(classOf[BucketGeoIndex[_, _]])
-        }
-        checkFilter(f, cqWithIndexes, spatialIndex)
+        checkFilter(f, cqWithIndexes)
       }
       s"return correct number of results for $name filter $f (various with geo-SRTree with default config)" >> {
-        if (checkFilterType) {
-          spatialIndex = Option.apply(classOf[WrappedSTRtree[_]])
-        }
-        checkFilter(f, cqWithSTRtreeIndexes, spatialIndex)
+        checkFilter(f, cqWithSTRtreeIndexes)
       }
       s"return correct number of results for $name filter $f (various with geo-SRTree with nodecapacity=20)" >> {
-        if (checkFilterType) {
-          spatialIndex = Option.apply(classOf[WrappedSTRtree[_]])
-        }
-        checkFilter(f, cqWithSTRtreeNodeCapacityIndexes, spatialIndex)
+        checkFilter(f, cqWithSTRtreeNodeCapacityIndexes)
       }
       s"return correct number of results for $name filter $f (various with QuadTree)" >> {
-        if (checkFilterType) {
-          spatialIndex = Option.apply(classOf[WrappedQuadtree[_]])
-        }
-        checkFilter(f, cqWithQuadtreeIndexes, spatialIndex)
+        checkFilter(f, cqWithQuadtreeIndexes)
       }
     }
   }
 
-  def runFilterTests(name: String, filters: Seq[Filter], checkFilter: Boolean): Fragments = Fragments(buildFilterTests(name, filters, checkFilter): _*)
+  def runFilterTests(name: String, filters: Seq[Filter], checkFilter: Boolean): Fragments = Fragments(buildFilterTests(name, filters): _*)
 
   "GeoCQEngine" should {
     runFilterTests("equality", equalityFilters, false)
